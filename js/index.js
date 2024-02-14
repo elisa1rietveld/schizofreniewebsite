83be7392b7418f794b0f155b98e0ba2abe37f60d
ZicoBacker <150025315+ZicoBacker@users.noreply.github.com>
2024-02-14 16:48:11 +0100

    fixed
    I wanna die

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById('mySideNav').style.width = '250px';
+    console.log('clicked');
+}
+
+function closeNav() {
+    document.getElementById('mySideNav').style.width = '0px';
+}
