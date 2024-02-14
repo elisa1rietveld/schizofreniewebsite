ebbc051bd97d6de9cf25c00ebd69722b4140f18c
ZicoBacker <150025315+ZicoBacker@users.noreply.github.com>
2024-02-14 16:21:53 +0100

    Revert "inital commit layla"
    This reverts commit 994a982a8fd7e9fbb3c7d8c75d9bd2531aaa52e6, reversing
    changes made to c5f0b070999ece7bb01e11a7164d4d97f2079c04.

--- js/index.js
+++
@@ -1,8 +0,0 @@
-function openNav() {
-    document.getElementById('mySideNav').style.width = '250px';
-    console.log('clicked');
-}
-
-function closeNav() {
-    document.getElementById('mySideNav').style.width = '0px';
-}
