5e636173ed7305465ae38eacef620142590ab5e6
ZicoBacker <150025315+ZicoBacker@users.noreply.github.com>
2024-03-04 16:19:59 +0100

    php migrate
    -extension changed to .php
    -seperate header added

---
+++ zelfTest.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Didact+Gothic&family=Mulish:wght@455&family=Protest+Revolution&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/zelfTest.css">
+    <title>Document</title>
+</head>
+    <?php include_once 'php/header.php';?>
+<body>
+
+    <script src="js/index.js"></script>
+    
+</body>
+</html>
