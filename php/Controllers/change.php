8554e1bbe6f491b275bf35aca8928f23c8810c89
ZicoBacker <150025315+ZicoBacker@users.noreply.github.com>
2024-03-06 20:41:05 +0100

    verify
    - due to having to verify users and passwords alot I made a Verify object with which we can verify passwords and usernames. This will make code a bit cleaner -Z

--- php/Controllers/change.php
+++ php/Controllers/change.php
@@ -1,31 +1,34 @@
 <?php
-require 'Lib/database.php';
+require_once 'Lib/database.php';
+require_once 'Lib/Verify.php';
 session_start();
 $db = new Database();
+$verify = new Verify();
 $done = NULL;
 
 if (!isset($_SESSION['user'])) {
     header('refresh: 0, url=/index.php');
 }
 
-var_dump($_POST);
 if (isset($_POST['newPassword']) && isset($_POST['oldPass'])) {
-    $newhashpass = password_hash($_POST['newPassword'], PASSWORD_BCRYPT);
-    var_dump($newhashpass);
-    $db->Query('UPDATE Users
-                SET password = :pass
-                WHERE username = :username;');
-    $db->Bind(':username', $_SESSION['user']);
-    $db->Bind(':pass', $newhashpass);
+    // checks if old password is the same as database password
+    if($verify->Pass($_SESSION['user'],$_POST['oldPass'])) {
+        // changes password to the new one.
+        $newhashpass = password_hash($_POST['newPassword'], PASSWORD_BCRYPT);
+        $db->Query('UPDATE Users
+                    SET password = :pass
+                    WHERE username = :user;');
+        $db->Bind(':user', $_SESSION['user']);
+        $db->Bind(':pass', $newhashpass);
+        $db->execute();
+    }
+    // checks if all went well
     
-    // current problem is that it will always return true. Should not be the case. Fix later
-    $result = $db->execute();
-    
-    if ($result) {
+    if ($verify->pass($_SESSION['user'],$_POST['newPassword'])) {
         $done = TRUE;
         return header('Refresh: 2, url=/php/profile.php');
     }
     else {
-        echo 'error';
+        return 'error';
     }
 }
---
+++ php/Lib/Verify.php
@@ -0,0 +1,46 @@
+<?php
+include_once 'database.php';
+Class Verify
+{
+    private Database $db;
+
+    public function __CONSTRUCT() {
+        $this->db = new Database();
+    }
+    // verifies password
+    public function Pass($user,$pass) {
+        // gets database password
+        $this->db->Query('SELECT password
+                          FROM Users
+                          WHERE username = :username;');
+        $this->db->Bind(':username', $user);
+        $result = $this->db->single()->password;
+
+        // checks if $pass is the same as database password
+        if(isset($result) && password_verify($pass, $result)) {
+            return TRUE;
+        } else {
+            return FALSE;
+        }
+    }
+
+    // verifies Usernames
+    public function User($user) {
+        // gets username
+        $this->db->Query('SELECT username
+                          FROM Users
+                          WHERE username = :username;');
+        $this->db->Bind(':username',$user);
+        $result = $this->db->single();
+
+        // checks if $user and database username are the same, 
+        // which it already did as it had something in there but still. We have to be sure!!!
+        if($user == $result) {
+            return  TRUE;
+        } else {
+            return FALSE;
+        }
+    }
+
+}
+
