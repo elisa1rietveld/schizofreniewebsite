dd97d8ca66f872ed95b8569580a5e8c9e1f51bc5
Laylab1989 <layla.b@mail.com>
2024-03-12 09:57:46 +0100

    layla contact position

--- contact.php
+++ contact.php
@@ -18,15 +18,18 @@ session_start();
     <h1 class="content">Contact</h1>
     <main>
     <div class="parent">
-     <div class="container">
-        <h1>Helplines</h1>
-        <div class="contact-info">
+     <div class="container ">
+         <img src="img/phone-call.png" alt="Phone Call" id="phone" >
+         <h1>Hulplijnen</h1>
+         <div class="contact-info">
+             <p>H B Hulplijnen:</p>
+            <a href="tel:0880767000">088 0767 000</a>
+
             <p>De Luisterlijn:</p>
             <a href="tel:0880767000">088 0767 000</a>
 
             <p>MIND Korrelatie:</p>
             <a href="tel:09001450">(0900) 1450</a>
-
             <p>Stichting 113 Zelfmoordpreventie:</p>
             <a href="tel:09000113">(0900) 0113</a>
         </div>
@@ -34,23 +37,23 @@ session_start();
         
 
         <div class="container">
+            <img src="img/user.png" alt="user">
             <h1>Contact</h1>
+            <!-- <div class="container"> -->
             <div class="contact-info">
-                    <p>Email:</p>
-                    <a href="mailto:schizofrenie@gmail.com">schizofrenie@gmail.com</a>
-
-                    <p>Telefoon:</p>
-                    <a href="tel:0880956000">088 0956 000</a>
-
-                    <p>Adres:</p>
-                    <p>Soestwetering 1, 3543 AZ Utrecht</p>
+                    <p>Contact:</p>
+                    <a href="mailto:schizofrenie@gmail.com">Email: schizofrenie@gmail.com</a>
+                    <a href="tel:0880956000">Telefoon: 088 0956 000</a>
+                <p class="address">Adres:</p>
+                <p class="address">Soestwetering 1, 3543 AZ Utrecht</p>
             </div>
-        </div>
+        <!-- </div> -->
 
+    </div>
     </div>
     </main>
 
-    <script src="js/index.js"></script>
+        <script src="js/index.js"></script>
     
     </body>
     </html>
