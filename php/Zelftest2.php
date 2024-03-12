e47cf9395685a4b7b8fbd65f39b7a8bff9804dde
ZicoBacker <150025315+ZicoBacker@users.noreply.github.com>
2024-03-12 09:24:50 +0100

    color changes
    added some color adjust for different pages

--- php/Zelftest2.php
+++ php/Zelftest2.php
@@ -13,6 +13,7 @@ function setQ($user,$Qnum, $value) {
                 ON Q.UserId = U.UserId
                 SET Q.Q:num = :value
                 WHERE U.username = :usr;');
+
     $db->bind(':num',$qnum);
     $db->bind(':value',$value);
     $bd->bind(':usr',$user);
@@ -26,20 +27,24 @@ function setQ($user,$Qnum, $value) {
 
 }
 
-
-
 $qMade = 0;
 if       (isset($_POST['Q1'])) {
-    $qMade = 1;
-    setQ($_SESSION['userId'], $qMade, $_POST['Q1']);
+  $qMade = 1;
+  setQ($_SESSION['userId'], $qMade, $_POST['Q1']);
 
 } elseif (isset($_POST['Q2'])) {
-    $qMade = 2;
+  $qMade = 2;
+  setQ($_SESSION['userId'], $qMade, $_POST['Q2']);
+
 } elseif (isset($_POST['Q3'])) {
   $qMade = 3;
+  setQ($_SESSION['userId'], $qMade, $_POST['Q3']);
 } elseif (isset($_POST['Q4'])) {
   $qMade = 4;
+  setQ($_SESSION['userId'], $qMade, $_POST['Q4']);
+
 } elseif (isset($_POST['Q5'])) {
   $qMade = 5;
+  setQ($_SESSION['userId'], $qMade, $_POST['Q5']);
 }
 
