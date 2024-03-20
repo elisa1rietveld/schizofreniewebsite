360649fe0ac30c514f679357f79d84fd5b806352
ZicoBacker <150025315+ZicoBacker@users.noreply.github.com>
2024-03-20 15:49:40 +0100

    Admin menu changes
    Today was a small and chill day just figuring out what is a smart idea
    - when changing user info, you cant scroll anymore
    - you can close the menu
    - created options to edit, with their own part
    - started working on the backend of the menu
    - some scss edits for nice looks

--- js/Admin.js
+++ js/Admin.js
@@ -3,6 +3,9 @@ const menuEL = document.querySelector('.passChange');
 const rowEl = document.querySelectorAll('tr.user');
 const menuNameEL = document.querySelector('h3.username');
 const closeButton = document.querySelector('span#cross');
+const body = document.querySelector('body');
+const userIn = document.querySelector('input#user');
+
 
 rowEl.forEach((element) => {
     //variable for name and the change element within each row.
@@ -12,13 +15,39 @@ rowEl.forEach((element) => {
     // opens menu when change button is pressed
     changeEl.addEventListener('click',() =>{
             menuEL.style.display = 'flex';
-            menuEL.style.opacity = 1;
-            menuNameEL.innerHTML = 'Change pass for ' + name;
+            body.style.overflow = 'hidden';
+            menuNameEL.innerHTML = 'User options for ' + name;
+            userIn.value = name;
+
     });
 });
 
 // closes menu when close button is pressed.
 closeButton.addEventListener('click', () => {
-        menuEL.style.display = 'none'
-        menuEL.style.opacity = 0;
+        menuEL.style.display = 'none';
+        body.style.overflow = 'auto';
+        userIn.value = 'default';
+
 })
+
+
+
+
+
+const selectEl = document.querySelector('select#choice');
+const pass = document.querySelector('#pass');
+const userRole = document.querySelector('#userRole');
+
+selectEl.addEventListener('change',() => {
+
+    if (selectEl.value == 'pass') {
+        userRole.classList.remove('active');
+        pass.classList.add('active');
+
+
+    } else if (selectEl.value == 'userRole') {
+        pass.classList.remove('active');
+        userRole.classList.add('active');
+    }
+})
+
--- php/Controllers/update.php
+++ php/Controllers/update.php
@@ -1,4 +1,16 @@
 <?php
-$UserId = $_GET['url'];
+require_once('../lib/database.php');
+require_once('../lib/verify.php');
+var_dump($_POST);
 
-echo "Hello user " . $UserId;
+$db = new Database();
+$verify = new Verify();
+//acoustic thing of me where I like to name it here for now.
+$choice = $_POST['choice'];
+
+
+if ($choice == 'userRole') {
+    
+} else if ($choice == 'password') {
+
+}
